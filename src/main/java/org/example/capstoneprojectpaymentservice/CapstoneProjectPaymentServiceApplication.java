3ba24ed64fc7f6b80a07f8c39ae4381806899f47
Priya Sharma <you@example.com>
2024-03-27 16:48:18 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/main/java/org/example/capstoneprojectpaymentservice/CapstoneProjectPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.capstoneprojectpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProjectPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/capstoneprojectpaymentservice/CapstoneProjectPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.capstoneprojectpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
